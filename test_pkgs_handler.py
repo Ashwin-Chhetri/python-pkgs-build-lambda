0a5aaac48dbf9c039e91a56e544e2dc0229be19b
Sachin Lal <sachinlal2016@u.northwestern.edu>
2018-03-01 12:31:25 -0600

    added changes to build.sh and readme

---
+++ test_pkgs_handler.py
@@ -0,0 +1,29 @@
+import os
+import ctypes
+
+for d, _, files in os.walk('lib'):
+    for f in files:
+        if f.endswith('.a'):
+            continue
+        ctypes.cdll.LoadLibrary(os.path.join(d, f))
+
+
+import scipy
+from scipy import special
+from ampercore.objects.metric import StateType
+import logging
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+    logger.info(special.gammaln(12345))
+    logger.info(StateType.Lost)
+    a = np.array(10)
+    logger.info(a)
+    d = {'test': list(range(10))}
+    df = pd.DataFrame(data=list(range(10)))
+    logger.info(df)
+    return {'yay': 'done'}
